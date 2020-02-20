f9bd9f4225c6e8b90130f83dbd9fe049c0a91ee9
AlGoulas <50454215+AlGoulas@users.noreply.github.com>
2020-02-20 09:32:05 +0100

    Add files via upload

---
+++ functions/ConnectCorticalSheet.m
@@ -0,0 +1,80 @@
+%The function receives as input a 2D sheet and Areas and reconnects
+%the neurons conained in the Sheet unti the desity of the area-to-area
+%matrix reaches the network_density_threshold value. 
+%
+%This is a static mode of conenctivity formation since neurons are
+%simultaneously present in the sheet and not added in a spatial or temporal
+%fashion.
+%
+%Input:
+%
+%Sheet:         A 2D MxN matrix
+%               Each entry of the 2D matrix Sheet is a 
+%               positive integer denoting the number of neurons that occupy
+%               each brain patch (in the synthetic brain).
+%
+%Areas:         A 2D matrix with unique integers denoting each area.
+%
+%network_density_threshold:
+%
+%               The connection denity of the matrix functioning as a
+%               stopping criterion for the connectivity formation between
+%               the neurons of the Sheet.
+%
+%distance_to_connect:
+%
+%               Positive float specifying the minimum distance 
+%               between an axon and a neuron that can lead to an 
+%               establishment of a connection  
+%
+%occupancy_thres:
+%
+%               Positive integer denoting how many 
+%               in-connections a neuron can establish
+%
+%Output:
+%
+%AllConnList:   1 x nr of time points cell containing all the structs 
+%               (ConnList returned from ConnectNeurons.m) specifying the 
+%               connections established at each time point of the 
+%               simulation
+%
+%Occupancy:     A 2D matrix NxM denoting how many in-connections 
+%               can be established per patch
+%
+%Established:   A 2D matrix NxM denoting how many in-connections 
+%               are established per patch
+%
+%--------------------------------------------------------------------------
+
+function [AllConnList, Occupancy, Established]=ConnectCorticalSheet(Sheet, Areas, network_density_threshold, occupancy_thres, distance_to_connect)
+
+Occupancy=zeros(size(Sheet));
+Established=zeros(size(Sheet));
+
+iter=1;
+AllConnList={};
+
+
+while 1
+
+    fprintf('\nIter:%d\n',iter);
+     
+    [ConnList, Established, Occupancy]=ConnectNeurons(Sheet, Occupancy, Established, occupancy_thres, distance_to_connect);%Careful with how the arguments are passed!
+    AllConnList{iter}=ConnList;
+    
+    %[C]=RunStatsOnAreaConnection(AllConnList, Sheet, Areas, length(unique(Areas)), [], 0);
+    [C,~]=AssembleAreaWiseMatrix(AllConnList, Areas);
+    kden=density_dir(C);
+
+    if(kden >= network_density_threshold)
+       break; 
+    end
+
+    iter=iter+1;
+
+
+end
+
+
+end
---
+++ functions/TasselatePoints2Areas.m
@@ -0,0 +1,53 @@
+%The function creates a 2D cortical sheet of x and y dimensions with the
+%corresponding size and a specified number of areas.
+%--------------------------------------------------------------------------
+%Input:    sheetxy: A 1d vector with 2 entries specifying the
+%                   X and Y dimensions of the cortical sheet 
+%                   e.g., [20 20]     
+%          n_points:An integer specifying the number of areas that the
+%                   cortical sheet will be composed of.
+%Output:   Areas:   A 2D matrix with unique integers denoting each area.
+%          XY:      The 2D coordinates of the centers of mass of the areas. 
+%--------------------------------------------------------------------------
+
+
+function [Areas, XY]=TasselatePoints2Areas(sheetxy,n_points)
+
+Areas=zeros(sheetxy(1),sheetxy(2));
+
+while 1
+
+x=floor(sheetxy(1).*rand(n_points,1))+1;
+y=floor(sheetxy(2).*rand(n_points,1))+1;
+
+XY=[x y];
+
+%So x y are the center of the areas. Assign now all the points to one of
+%these areas.
+
+indexes=1:size(XY,1);
+
+[x_sheet y_sheet]=find(~isnan(Areas));
+
+for i=1:size(XY,1)
+    
+    distances(:,i)=sqrt(((XY(i,1)-x_sheet).^2)+((XY(i,2)-y_sheet).^2));
+
+end
+
+for i=1:size(distances,1)
+    
+    tmp=find(distances(i,:)==min(distances(i,:)));
+    ids(i)=tmp(1);
+    
+end
+
+Areas(sub2ind(size(Areas),x_sheet,y_sheet))=ids;
+
+if(length(unique(Areas))==n_points)
+    break;
+end
+
+end
+
+return
